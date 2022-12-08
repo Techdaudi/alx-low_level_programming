1164ff5a363a7a126d41b4d611b62acaacc2bd5e
Daudi <techdaudi@gmail.com>
2022-12-08 09:52:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,11 @@
  * Main - Prints string
  * Return: returns 0 if successful otherwise 1
  */
+
 int main(void)
 {
-    char char1[] = "with proper grammar, but the outcome is a piece of art,";
-    printf("%s",char1);
+	char char1[] = "with proper grammar, but the outcome is a piece of art, ";
+	printf("%s",char1);
 
-    return (0);
+	return (0);
 }
