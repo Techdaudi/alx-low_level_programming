f78f7275e27a0fb01d6eee4c3d24a877fa32c6f8
Daudi <techdaudi@gmail.com>
2022-12-08 09:30:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+/**
+ * Main - Prints string
+ * Return: returns 0 if successful otherwise 1
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,");
+
+    return (0);
+}
