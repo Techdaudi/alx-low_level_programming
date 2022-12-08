ffc24794a56be7c1e5d60f00be1dd2ceda0c49e9
Daudi <techdaudi@gmail.com>
2022-12-08 10:32:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 /**
- * main - function to print a string
- * @string1 - string to be printed
- * Return 0 if successful 1 otherwise
+ * main - Function to print a string
+ * @string1: string to be printed
+ * Return: Returns 0 if successful 1 otherwise
  */
 
 int main(void)
 {
 	/*Func description*/
-	char string1[] = "\"Programming is like building a multilingual puzzle\n";
+	char string1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string1);
 
