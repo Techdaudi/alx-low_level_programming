f7aab26256ca45ebba3ef60d6c9f876da08135fc
Daudi <techdaudi@gmail.com>
2022-12-08 08:51:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Function to perint a string 
+ */
 int main(void)
 {
-        /*Function to perint a string*/
 
 	char string[] = "\"Programming is like building a multilingual puzzle";
 
