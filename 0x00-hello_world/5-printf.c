a36b506b2e66ea733b890bb1c667b57304ae1ded
Daudi <techdaudi@gmail.com>
2022-12-08 10:17:47 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 /**
- * main - funtciont that prints string
+ * main - funtcion that prints string
  * @char1 - string to be printed.
  * Return: returns 0 if successful otherwise 1
  */
 
 int main(void)
 {
+	/*Func description*/
 	char char1[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", char1);
