8cb65907694b7d00b239a0f3284513665b91c430
Daudi <techdaudi@gmail.com>
2022-12-09 17:38:33 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Function to print the alphabet
+*
+*Return: prints 0 is successful otherwse 1.
+*/
+
+int main(void)
+{
+        /*Print the alphabet*/
+        int c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+		putchar(toupper(c));
+        }
+        putchar('\n');
+        return (0);
+}
