fc821443739cfa57b7d767a484e9ad97f6c31650
Daudi <techdaudi@gmail.com>
2022-12-09 17:53:56 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	/*Print the alphabet*/
 	int c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-                putchar(toupper(c));
-        }
+		putchar(toupper(c));
+	}
 	putchar('\n');
 	return (0);
 }
