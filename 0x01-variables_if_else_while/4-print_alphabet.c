37640912e03c93b9bf499b5b9055ea9f26db8908
Daudi <techdaudi@gmail.com>
2022-12-09 18:07:56 -0800

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - Function to print the alphabet
+*
+*Return: prints 0 is successful otherwse 1.
+*/
+
+int main(void)
+{
+	/*Print the alphabet*/
+	int c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c = 'q' || c = 'e')
+		{
+			break;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
