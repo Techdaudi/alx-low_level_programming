c928f7d9699e74e8c0150c360d36d03215e6c388
Daudi <techdaudi@gmail.com>
2022-12-10 21:15:39 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reverse of lowercase letters
+ * Return: Returns o if successful 1 otherwise
+ */
+
+int main(void)
+{
+	/*Prints loswercase alphabet in reverse*/
+	int n = 0;
+	char ch;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n%10) + '0');
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
