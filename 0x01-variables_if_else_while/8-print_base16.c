121629e37cc6b89d4b1bf6f2c8dd64c980063c7a
Daudi <techdaudi@gmail.com>
2022-12-10 21:22:34 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int n;
 	char ch;
 
-	for (n = 1; n <= 9; ++n)
+	for (n = 0; n <= 9; ++n)
 	{
-		putchar((n%10) + '0');
+		putchar((n % 10) + '0');
 	}
 	for (ch = 'a'; ch <= 'f'; ++ch)
 	{
