fab349f452bf0a6e18cd21f762ded080581daea5
Daudi <techdaudi@gmail.com>
2022-12-13 06:18:50 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,17 @@ int main(void)
 	int x;
 
 	for (x = 0; x <= 9; ++x)
-		{
-			putchar((x) + '0');
+	{
+		putchar((x) + '0');
 
-			if (x == 9)
-				continue;
-
-			putchar(',');
-
-			putchar(' ');
-		}
-
+		if (x == 9)
+			continue;
+		
+		putchar(',');
+		
+		putchar(' ');
+	}
+	
 	putchar('\n');
 
 	return (0);
