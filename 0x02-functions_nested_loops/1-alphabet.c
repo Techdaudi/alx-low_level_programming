2a228064a023af0025107f5fbb565d41b4b1e795
Daudi <techdaudi@gmail.com>
2022-12-14 09:15:48 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+void print_alphabet(void);
+
+/**
+*main - Calls the function  print_alphabet.
+*Return 0 if successful otherwise 0.
+*/
+
+int main(void)
+{
+	/*Call functon print_alphabet*/
+	char c2;
+
+	c2 = print_alphabet(void);
+
+	_putchar(c2);
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	/*Prints lowercase alphabet*/
+	char c;
+
+	for (c = 'a';c <= 'z', ++c)
+		_putchar(c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _putchar(char c);
 
 int print_string(void);
+
+void print_alphabet(void);
