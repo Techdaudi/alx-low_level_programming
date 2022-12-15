8bb14f0a26a87979c7e02bc629437bc7c2d80831
Daudi <techdaudi@gmail.com>
2022-12-15 03:43:24 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,31 +2,22 @@
 void print_alphabet_x10(void);
 
 /**
-*main - Calls the function  print_alphabet.
+*print_alphabet_x10 - Calls the function  print_alphabet.
 *Return 0 if successful otherwise 0.
 */
-
-int main(void)
-{
-	/*Call functon print_alphabet*/
-	int d;
-
-	for (c = 1;c <= 10, ++d);
-	{
-		print_alphabet();
-	}
-
-	return (0);
-}
 
 void print_alphabet_x10(void)
 {
 	/*Prints lowercase alphabet*/
 	char c;
+	int n = 1;
 
-	for (c = 'a';c <= 'z'; ++c);
+	while (n <= 10)
 	{
-		_putchar(c);
+		for (c = 'a';c <= 'z'; ++c);
+		{
+			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
