474ffbe7c1fe2735f230a0e6bcfd411722c582cd
Daudi <techdaudi@gmail.com>
2022-12-15 11:00:02 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int mul(int a, int b);
+
+/**
+*mul - Prints absolute valu of number.
+*@a: parameter to be retuned
+*@b: parameter to be retuned
+*return: 0 if successful otherwise 1.
+*/
+
+int mul(int a, int b)
+{
+	/*Multiply 2 numbers*/
+	int result;
+
+	result = a * b;
+
+	return result;
+}
