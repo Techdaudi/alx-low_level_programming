aff9c86f20599a6f9e703f753f195f1a03fb85a9
Daudi <techdaudi@gmail.com>
2022-12-15 05:53:10 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 int _isalpha(int c);
 
 /**
-*int _isalpha - Calls the function  print_alphabet.
-*Return 0 if successful otherwise 1.
+*_isalpha - Calls the function print_alphabet.
+*return:  1 if successful 0 otherwise.
 */
 
 int _isalpha(int c)
 {
 	/*Checks if character is alphabet*/
+	int c;
 
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
