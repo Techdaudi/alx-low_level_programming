d516a0088c35ef2c17d95823bd5c560674ae0222
Daudi <techdaudi@gmail.com>
2022-12-15 08:35:35 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int _isupper(int c);
+
+/**
+*_isupper - Prints absolute valu of number.
+* @c: parameter to be retuned
+*return: 0 if successful otherwise 1.
+*/
+
+int _isupper(int c)
+{
+	/*Checks for digit*/
+	if (c >= 'A'; c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
