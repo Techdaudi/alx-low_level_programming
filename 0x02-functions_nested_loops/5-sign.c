0f592546d2c591e1bc9e46c1bad661622d4bd6b7
Daudi <techdaudi@gmail.com>
2022-12-15 06:52:38 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar(n%1 + '0');
+		_putchar('+' + '0');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar(n%1 + '0');
+		_putchar('0' + '0');
 	}
 	else
 	{
 		return (-1);
-		_putchar(n%1 + '0');
+		_putchar('-1' + '0');
 	}
 	_putchar('\n');
 }
