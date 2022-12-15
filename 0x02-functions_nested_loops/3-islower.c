7b3cab1b551e7315e6ee8ff42bb2ba01d13933a8
Daudi <techdaudi@gmail.com>
2022-12-15 04:18:42 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+int _islower(int c);
+
+/**
+*_islower - Calls the function  print_alphabet.
+*Return 0 if successful otherwise 0.
+*/
+
+int _islower(int c)
+{
+	/*Checks if character is lower case*/
+	int n1 = 0;
+	int n2 = 1;
+
+	if (c <= 'a' && c <= 'z')
+	{
+		_putchar(n1);
+	}
+	else
+	{
+		_putchar(n2);
+	}
+}
