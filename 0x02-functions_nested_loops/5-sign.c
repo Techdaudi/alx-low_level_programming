e435f4d4b85bccb12c98aabf06addf74fb0322e2
Daudi <techdaudi@gmail.com>
2022-12-15 06:17:41 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int print_sign(int n);
+
+/**
+*print_sign - Calls the function  print_alphabet.
+* @n: parameter to be retuned
+*Return 0 if successful otherwise 1.
+*/
+
+int print_sign(int n)
+{
+	/*Prints sign of a number*/
+	if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (1);
+		_putchar('+');
+	}
+	_putchar('\n');
+}
