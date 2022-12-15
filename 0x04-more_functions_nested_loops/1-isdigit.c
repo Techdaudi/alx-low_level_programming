f19cd871b88daa4835b39a2f5d990018091b780a
Daudi <techdaudi@gmail.com>
2022-12-15 08:16:22 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int _isdigit(int c);
+
+/**
+*_isdigit - Prints absolute valu of number.
+* @c: parameter to be retuned
+*return: 0 if successful otherwise 1.
+*/
+
+int _isdigit(int c)
+{
+	/*Checks for digit*/
+	if (c >= 0 && c <=9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
