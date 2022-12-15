ba0fa79cd9c2793e8b8a50a869c84d88fc561c77
Daudi <techdaudi@gmail.com>
2022-12-15 03:51:45 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,10 @@ void print_alphabet_x10(void)
 
 	while (n <= 10)
 	{
-		for (c = 'a';c <= 'z'; ++c);
+		for (c = 'a';c <= 'z'; ++c)
 		{
 			_putchar(c);
+		_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
