b8ad9aec207715611dd89709f73ff10fc6ea8d42
Daudi <techdaudi@gmail.com>
2022-12-16 02:03:50 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,7 @@ void print_numbers(void);
 
 /**
 *mul - Prints absolute valu of number.
-*@a: parameter to be retuned
-*@b: parameter to be retuned
+*@n: parameter to be retuned
 *_putchar: 0 if successful otherwise 1.
 */
 
@@ -13,7 +12,7 @@ void print_numbers(void);
 	/* prints the numbers, from 0 to 9*/
 	int n;
 
-	for (n = 0; n <= 9; ++n)
+	if (n >= 0 && n <= 9)
 	{
 		_putchar(n + '0');
 	}
