a14b22b961fd4ae6f5dcaf94f52d0c8856bf6685
Daudi <techdaudi@gmail.com>
2022-12-16 03:44:50 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+void print_line(int n);
+
+/**
+*print_line - Prints absolute value of number.
+*@n: variable 1
+*Return: 0 if successful otherwise 1.
+*/
+
+void print_line(int n)
+{
+	/* prints underscore n times*/
+	int m;
+	
+	for (m = n; m <= n; ++m)
+	{
+		if (m <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
