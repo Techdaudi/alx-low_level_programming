858081e5fbcee5991a31c9b7fcfd61aa6332b50b
Daudi <techdaudi@gmail.com>
2022-12-16 03:15:16 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,22 @@ void more_numbers(void);
 void more_numbers(void);
 {
 	/* prints 10 times the numbers 0 to 14*/
-	char c = '0';
-	int n = 1;
+	int n1 = 0;
+	int n2 = 0;
 
-	while (n <= 10)
+	while (n1 <= 9)
 	{
-		while (c <= '14')
-			_putchar(c);
-		_putchar('\n');
-		c++;
-	}
+		while (n2 <= 14)
+		{
+			if (2 >= 10)
+			{
+				_putchar(n2 / 10 + '0');
+			_putchar(n2 % 10 + '0');
+			n1++;
+			}
+		}
 	_putchar('\n');
+	n1++;
+	n1 = 0;
+	}
 }
