634b4b46f88c9e3c80e2ed1b5b79a75c1b5833c2
Daudi <techdaudi@gmail.com>
2022-12-16 02:12:57 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,9 @@ void print_numbers(void);
 void print_numbers(void);
 {
 	/* prints the numbers, from 0 to 9*/
-	int n;
+	char n;
 
-	for (n >= 48; n < 57; n++)
+	for (n >= '0'; n < '9'; n++)
 	{
 		_putchar(n + '0');
 	}
