9ecd067e0448bc4454b93ff8e4f0df1a5f16be28
Daudi <techdaudi@gmail.com>
2022-12-16 01:46:36 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@ int mul(int a, int b);
 
 /**
 *mul - Prints absolute valu of number.
-*@a: parameter to be retuned
-*@b: parameter to be retuned
-*return: 0 if successful otherwise 1.
+*@a: argument 1.
+*@b: argument 2.
+*Return: 0 if successful otherwise 1.
 */
 
 int mul(int a, int b)
