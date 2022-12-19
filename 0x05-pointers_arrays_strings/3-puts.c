0a06580c0da3790806bb2e4a2a393a2350d03afd
Daudi <techdaudi@gmail.com>
2022-12-19 09:19:44 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str);
+
+/**
+*_puts - function to print str.
+*@str: a pointer int.
+*Return: nothing.
+*/
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar("\n");
+}
