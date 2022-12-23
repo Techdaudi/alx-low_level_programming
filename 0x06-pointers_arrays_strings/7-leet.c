6509a91d122a52b798ddcc3dc328da4187b1e553
Daudi <techdaudi@gmail.com>
2022-12-23 08:57:37 -0800

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,25 +8,20 @@ char *leet(char *s);
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
 
+	char numbers[5] = {'4', '3', '0', '7', '1'};
+	char letters[5] = {'A', 'E', 'O', 'T', 'L'};
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-		else
+		for (j = 0; j < 5; j++)
 		{
-			s[i] = s[i];
+			if (s[i] = letters[j] || s[i] - 32 = numbers[j])
+			{
+				s[i] = numbers[j];
+			}
 		}
 	}
-
-	return (s);
+retrun (s);
 }
