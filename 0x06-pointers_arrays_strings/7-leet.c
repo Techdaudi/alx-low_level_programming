d46139a3f982174a3340c4f6244dee3afc28e039
Daudi <techdaudi@gmail.com>
2022-12-23 08:38:48 -0800

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,32 @@
 #include "main.h"
-char *leet(char *);
+char *leet(char *s);
 /**
 *leet - function that encodes a string into 1337.
 *@s: string.
 *@s2: string 2
 *Return: Always zero.
 */
-char *leet(char *)
+char *leet(char *s)
 {
 	int i;
-	char *s;
-	char *s2;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == 'a' || s[i] == 'A')
-			s2[i] = '4';
+			s[i] = '4';
 		else if (s[i] == 'e' || s[i] == 'E')
-			s2[i] = '3';
+			s[i] = '3';
 		else if (s[i] == 'o' || s[i] == 'O')
-			s2[i] = '0';
+			s[i] = '0';
 		else if (s[i] == 't' || s[i] == 'T')
-			s2[i] = '7';
+			s[i] = '7';
 		else if (s[i] == 'l' || s[i] == 'L')
-			s2[i] = '1';
+			s[i] = '1';
 		else
 		{
-			s2[i] = s[i];
+			s[i] = s[i];
 		}
 	}
 
-	return (s2);
+	return (s);
 }
