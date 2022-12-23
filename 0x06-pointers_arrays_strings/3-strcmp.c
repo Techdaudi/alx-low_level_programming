81d3bc31d744b7a7ea3a410b50cdfb180f894773
Daudi <techdaudi@gmail.com>
2022-12-23 07:37:31 -0800

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,42 @@
 int _strcmp(char *s1, char *s2);
 
 /**
-*_strcmp - function that compares 2 strings.
-*@s1: destination string
-*@s2: source string
-*Return: 0.
-*/
+  *_strcmp - function that compares 2 strings.
+  *@s1: destination string
+  *@s2: source string
+  *	
+  *Return: 0.
+  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j, k;
+	int res1, res2;
 	
-	for (i = 0; s1[i] != '\0'; ++i)
+	for (i = 0; s1[i] != '\0'; ++i);
+
+	for (i = 0; s2[j] != '\0'; ++j);
+
+	if (i <= k)
 	{
-		if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else
-			return (s1[i] - s2[i]);
+		res1 = i;
 	}
-	return (0);
+	else
+	{
+		res1 = j;
+	}
+
+	for (k = 0; k <= res1; k++)
+	{
+		if s1[k] == s2[k]
+		{
+			continue;
+		}
+		else
+		{
+			res2 = s1[k] - s2[k];
+			break;
+		}
+	}
+
+	return (res2);
 }
